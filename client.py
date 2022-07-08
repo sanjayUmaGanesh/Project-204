be715b627ec5c80ca924e67867aeb0648867a69c
sanjayUmaGanesh <69663787+sanjayUmaGanesh@users.noreply.github.com>
2022-07-08 18:50:06 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,81 @@
+import socket
+from threading import *
+from tkinter import *
+from PIL import ImageTk, Image
+
+screen_width = None
+screen_height = None
+
+SERVER = None
+PORT = None
+IP_ADDRESS = None
+
+
+canvas1 = None
+
+playerName = None
+nameEntry = None
+nameWindow = None
+
+def askPlayerName():
+    global playerName
+    global nameEntry
+    global nameWindow
+    global canvas1
+    global screen_width
+    global screen_height
+
+    nameWindow = Tk()
+    nameWindow.title('TAMBOLA FAMILY FUN')
+    nameWindow.attributes('-fullscreen', True)
+
+    screen_width = nameWindow.winfo_screenwidth()
+    screen_height = nameWindow.winfo_screenheight()
+
+    bg = ImageTk.PhotoImage(file = './assets/background.jpeg')
+
+    canvas1 = Canvas(nameWindow, width = 500, height = 500)
+    canvas1.pack(fill = 'both', expand = True)
+    canvas1.create_image(350, 150,image = bg, anchor = "nw")
+
+    canvas1.create_text(screen_width/2.05, screen_height/5, text = "Enter name", font = ("Chalkboard SE", 100), fill = "white")
+
+    nameEntry = Entry(nameWindow, width = 15, justify = 'center', font = ("Chalkboard SE", 50), bd = 5, bg = 'white')
+    nameEntry.place(x = screen_width/2-280, y = screen_height/4+100)
+
+    button = Button(nameWindow, text = "SAVE", font = ("Chalkboard SE", 30), width = 15, command = saveName,
+    height = 1, bg = "#80DEEA", bd = 3 )
+    button.place(x = screen_width/2 - 205, y = screen_height/2-30)
+
+    nameWindow.resizable(True, True)
+    nameWindow.mainloop()
+
+
+def saveName():
+    global SERVER
+    global playerName
+    global nameWindow
+    global nameEntry
+
+    playerName = nameEntry.get()
+    nameEntry.delete(0, END)
+    nameWindow.destroy()
+
+    # Sending Message to Server
+    SERVER.send(playerName.encode())
+
+def setup():
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    PORT  = 5000
+    IP_ADDRESS = '127.0.0.1'
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS, PORT))
+
+    askPlayerName()
+
+setup()
+
